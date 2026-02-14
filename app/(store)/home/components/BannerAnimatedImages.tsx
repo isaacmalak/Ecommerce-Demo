4e67b59ce756e65c03fa235778a70992ee1a8717
Isaac Malak <isaacmalak123@gmail.com>
2026-02-14 18:17:14 +0200

    feat: Add BannerAnimatedImages component to display animated character figures on the home banner.

--- app/(store)/home/components/BannerAnimatedImages.tsx
+++ app/(store)/home/components/BannerAnimatedImages.tsx
@@ -2,36 +2,41 @@ import { BannerImage } from "./BannerImage";
 
 export function BannerAnimatedImages() {
   return (
-    <div className="relative hidden h-[180px] w-full flex-row overflow-hidden sm:flex md:h-[230px] md:w-1/2">
+    <div className="relative flex h-[200px] w-full flex-row overflow-hidden md:h-[230px] md:w-1/2">
+      {/* Superman - Center Right */}
       <BannerImage
-        position="right-34 top-7 z-10 "
+        position="right-10 top-5 w-24 z-10 md:right-34 md:top-7 md:w-[200px]"
         src={"/toys-figures/superman.png"}
         width={200}
         height={50}
       />
+      {/* Joker - Mid Left */}
       <BannerImage
-        position="top-0 right-52 "
+        position="right-20 top-0 w-28 md:right-52 md:top-0 md:w-[230px]"
         src={"/toys-figures/joker.png"}
         width={230}
         height={70}
         animationDelay={1.6}
       />
+      {/* Travis - Far Right */}
       <BannerImage
-        position="top-0 right-20 "
+        position="right-2 top-0 w-20 md:right-20 md:top-0 md:w-[150px]"
         src={"/toys-figures/travis.png"}
         width={150}
         height={70}
-        animationDelay={0.8}
+        animationDelay={1.9}
       />
+      {/* Billy - Far Left */}
       <BannerImage
-        position="top-7 right-79 z-10"
+        position="right-32 top-5 w-24 z-10 md:right-79 md:top-7 md:w-[200px]"
         src={"/toys-figures/billy.png"}
         width={200}
         height={70}
         animationDelay={1.5}
       />
+      {/* Homelander - Furthest Left */}
       <BannerImage
-        position="top-3 right-100"
+        position="right-44 top-2 w-30 md:right-100 md:top-3 md:w-[240px]"
         src={"/toys-figures/homelander.png"}
         width={240}
         height={70}
